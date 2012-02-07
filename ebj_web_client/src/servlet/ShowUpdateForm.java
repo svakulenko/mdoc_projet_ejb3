954eb5642350b27aff575840111f78685017107a
Nguyen Xuan Hoa <nxhoaf@gmail.com>
2012-02-07 18:11:31 +0100

    Update Contact

---
+++ ebj_web_client/src/servlet/ShowUpdateForm.java
@@ -0,0 +1,51 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ShowUpdateForm
+ */
+public class ShowUpdateForm extends BaseServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ShowUpdateForm() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String id = request.getParameter("id");
+		System.out.println("Updating contact " +  id + "....");
+		String dbOutput = daoContact.getUpdateForm(new Integer(id));
+		PrintWriter out = response.getWriter() ;
+
+	      
+	      out.println (getHeader(null));
+	      out.println (getBody("Show Update Form"));
+	      out.println (dbOutput);
+	         
+	      out.println (getFooter());
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
